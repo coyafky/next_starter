67e74dbf3114fc1b6b0c24ac7414ea9cf772b2ce
coya <2233606771@qq.com>
2024-04-20 11:04:38 +0800

    change

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+export default function Home(){
+  return (
+    <div>
+      <section className="py-24">
+        <div className="container">
+          <h1 className="text-center text-4xl font-bold mb-10">
+            hello
+          </h1>
+        </div>
+      </section>
+    </div>
+  )
+}
